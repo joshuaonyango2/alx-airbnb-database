aa435b1189c32a91a08338c2cd83670f402f7258
joshuaonyango2 <joshuaonyango372@gmail.com>
2025-05-19 20:31:47 +0300

    Update and rename subqueries.sql,README.md to subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,35 +1,61 @@
--- Task 1: Non-correlated subquery to find properties with average rating > 4.0
+**Task 1: Non-correlated subquery to find properties with average rating > 4.0**
 
 SELECT 
+    
     property_id,
+    
     name
+    
 FROM 
+    
     properties 
+    
 WHERE 
+    
     property_id IN (
+    
         SELECT 
+    
             property_id
+    
         FROM 
+    
             reviews 
+    
         GROUP BY 
+    
             property_id
+    
         HAVING 
+    
             AVG(rating) > 4.0
     );
 
 
--- Task 2: Correlated subquery to find users with more than 3 bookings
+**Task 2: Correlated subquery to find users with more than 3 bookings**
+    
 
 SELECT 
+    
     user_id,
+    
     name
+    
 FROM 
+    
     users 
+    
 WHERE (
-    SELECT 
+    
+    SELECT
+    
         COUNT(*)
+    
     FROM 
+    
         bookings 
+    
     WHERE 
-        b.user_id = user_id
+    
+        user_id = user_id
 ) > 3;
